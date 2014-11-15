f56197d2fbcc81be355fb5ddcfc9eec0ffcd4fec
Charlie Hanner <channer@mitre.org>
2014-11-14 22:37:48 -0500

    [puppet] Added base ubuntu packages

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -3,3 +3,7 @@ include admin::stages
 class { 'baseconfig':
   stage => 'pre_install'
 }
+
+class { 'ubuntu':
+  stage => 'base_install'
+}
--- puppet/modules/admin/manifests/stages.pp
+++ puppet/modules/admin/manifests/stages.pp
@@ -1,3 +1,4 @@
 class admin::stages {
-  stage { 'pre_install': before => Stage['main'] }
+  stage { 'pre_install': before   => Stage['main'] }
+  stage { 'base_install': require => Stage['pre_install'] }
 }
